2fd639a7088d7a89f5f96e4f33e7af69eb44b21c
Engel62 <sr.engel@bk.ru>
2023-03-02 15:08:26 +0300

    классы созданны

---
+++ src/main/java/me/engelsergey/recipes/controllers/model/Ingredient.java
@@ -0,0 +1,7 @@
+package me.engelsergey.recipes.controllers.model;
+
+public class Ingredient {
+    public static String name;
+    public static int numberOfIngredients;
+    public static String unitOfMeasurement;
+}
---
+++ src/main/java/me/engelsergey/recipes/controllers/model/Recipes.java
@@ -0,0 +1,12 @@
+package me.engelsergey.recipes.controllers.model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Recipes {
+    public static String name;
+    public static int cookingTime;
+    public static ArrayList<Ingredient>ingredients;
+    public static String[] cookingSteps;
+
+}
