de8819bd06e190fca718a79d43fc5d086b414457
Engel62 <sr.engel@bk.ru>
2023-03-02 15:37:05 +0300

    классы созданны

--- src/main/java/me/engelsergey/recipes/controllers/model/Ingredient.java
+++ src/main/java/me/engelsergey/recipes/controllers/model/Ingredient.java
@@ -1,5 +1,10 @@
 package me.engelsergey.recipes.controllers.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Ingredient {
     private String name;
     private int numberOfIngredients;
--- src/main/java/me/engelsergey/recipes/controllers/model/Recipes.java
+++ src/main/java/me/engelsergey/recipes/controllers/model/Recipes.java
@@ -1,8 +1,12 @@
 package me.engelsergey.recipes.controllers.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-
+@Data
+@AllArgsConstructor
 public class Recipes {
     private String name;
     private int cookingTime;
